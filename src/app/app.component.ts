2081c66fa269f1869de870d675155e72e69e7109
snehasoni16 <snehasoni996@gmail.com>
2025-03-04 23:42:00 +0530

    login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { StorageService } from './auth/components/services/storage/storage.service';
+import { Router } from '@angular/router';
+import { every } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Car_rental_system_angular';
+
+  isCustomerLoggedIn: boolean=StorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean=StorageService.isAdminLoggedIn();
+
+  constructor(private router: Router){}
+  ngOnInit(){
+    this.router.events.subscribe(event=>
+    {
+      if(event.constructor.name==="NavigationEnd"){
+        this.isAdminLoggedIn=StorageService.isAdminLoggedIn();
+        this.isCustomerLoggedIn=StorageService.isCustomerLoggedIn();
+      }
+    }
+    )
+  }
+  logout(){
+    StorageService.logout();
+    this.router.navigateByUrl("/login");
+  }
 }
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -3,6 +3,7 @@ import{FormBuilder,FormControl,FormGroup,Validators} from '@angular/forms';
 import { AuthService } from '../services/auth/auth.service';
 import{NzMessageService} from "ng-zorro-antd/message";
 import { Router } from '@angular/router';
+import { StorageService } from '../services/storage/storage.service';
 @Component({
   selector: 'app-login',
   standalone: false,
@@ -17,7 +18,9 @@ throw new Error('Method not implemented.');
 isSpinning : boolean=false;
 loginForm!:FormGroup;
 constructor(private fb:FormBuilder
-  ,private authService: AuthService
+  ,private authService: AuthService,
+  private router:Router,
+  private message : NzMessageService
 ){}
 ngOnInit(){
   this.loginForm=this.fb.group({
@@ -29,6 +32,23 @@ login(){
   console.log(this.loginForm.value);
   this.authService.login(this.loginForm.value).subscribe((res)=>{
     console.log(res);
+    if(res.userId!=null){
+      const user={
+        id:res.userId,
+        role:res.userRole
+      }
+      StorageService.saveUser(user);
+      StorageService.saveToken(res.jwt);
+     if(StorageService.isAdminLoggedIn()){
+       this.router.navigateByUrl("/admin/dashboard");
+      }else if(StorageService.isCustomerLoggedIn()){
+        this.router.navigateByUrl("/customer/dashboard");
+      }
+      else{
+        this.message.error("Bad credentials",{nzDuration:5000});
+  
+      }
+   }
   })
 }
 }
